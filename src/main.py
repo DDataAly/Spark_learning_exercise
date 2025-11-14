1660505a6aa08f8b6378ae6f6d6eb72fc753dabf
DDataAly <alyona.dorof@gmail.com>
2025-11-14 22:16:02 +0000

    Moves saving logic into TradingPlatform child class methods

--- src/main.py
+++ src/main.py
@@ -1,23 +1,18 @@
 from src.extract.binance_api_fetcher import Binance
-from src.utils.save_raw_data import save_json
 from src.utils.config_loader import load_config
 
 def main():
     config = load_config()
-    binance_config = config ['binance']
+    data_source = Binance(config)
 
-    data_source = Binance(
-                            name = 'binance',
-                            url = binance_config['base_url'],
-                            tickers = binance_config ['tickers'],
-                            interval = binance_config['interval']
-                            )
     for ticker in data_source.tickers:
-            data = data_source. get_ticker_data(ticker = ticker)
-            file_name = f"{data_source.name}_{ticker}_{data_source.interval}"
-            save_json(data, file_name, config)
+            print(f'Starting a fetch for for {ticker} from {data_source.name}')
+            data_source. get_historical_data(ticker = ticker)
+            print(f' Data for {ticker} from {data_source.name} is fetched and saved successfully')
+
 
 if __name__ == "__main__":
     main()
+
 
 
