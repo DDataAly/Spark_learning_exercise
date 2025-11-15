477b8799f524b91c12485ce04b72c078d1cc67fa
DDataAly <alyona.dorof@gmail.com>
2025-11-15 21:38:34 +0000

    Add logging to main.py

--- src/main.py
+++ src/main.py
@@ -1,18 +1,23 @@
 from src.extract.binance_api_fetcher import Binance
 from src.utils.config_loader import load_config
+import logging
+import src.utils.logger
+
+logger=logging.getLogger('main')
 
 def main():
     config = load_config()
     data_source = Binance(config)
 
     for ticker in data_source.tickers:
-            print(f'Starting a fetch for for {ticker} from {data_source.name}')
+            logger.info(f'Starting a fetch for for {ticker} from {data_source.name}')
             data_source. get_historical_data(ticker = ticker)
-            print(f' Data for {ticker} from {data_source.name} is fetched and saved successfully')
+            logger.info(f' Data for {ticker} from {data_source.name} is fetched and saved successfully')
 
 
 if __name__ == "__main__":
     main()
+    print('cats')
 
 
 
